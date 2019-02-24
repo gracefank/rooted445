6864a96a0e62af99b95604394374455414e4597b
charlie <charliekw@gmail.com>
2019-02-24 00:45:39 -0600

    fps limiting

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+var lastFrameTimeMs = Date.now(); // The last time the loop was run
+var maxFPS = 1; // The maximum FPS we want to allow
+var timestamp = 0;
 
 function update() {
     console.log(Date.now());
@@ -8,8 +11,15 @@ function draw() {
 
 
 function mainLoop() {
+    timestamp = Date.now();
+    if(timestamp < lastFrameTimeMs + (1000/maxFPS)){
+        requestAnimationFrame(mainLoop);
+        return;
+    }
+
     update();
     draw();
+    lastFrameTimeMs = timestamp;
     requestAnimationFrame(mainLoop);
 }
 
