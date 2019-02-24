d256f75277f9ba34ad3380f24cb42df8f2cd8b57
charlie <charliekw@gmail.com>
2019-02-23 23:56:22 -0600

    moving box

--- game.js
+++ game.js
@@ -1,2 +1,24 @@
+var box = document.getElementById('box'), // the box
+boxPos = 10, // the box's position
+boxVelocity = 2, // the box's velocity
+limit = 300; // how far the box can go before it switches direction
 
-console.log("hello world");
+function update() {
+    boxPos += boxVelocity;
+    // Switch directions if we go too far
+    if (boxPos >= limit || boxPos <= 0) boxVelocity = -boxVelocity;
+}
+
+function draw() {
+    box.style.left = boxPos + 'px';
+}
+
+
+function mainLoop() {
+    update();
+    draw();
+    requestAnimationFrame(mainLoop);
+}
+
+// Start things off
+requestAnimationFrame(mainLoop);
