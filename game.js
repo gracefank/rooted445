6ba7257a57951bcf54c416b25abb75a6868864cd
charlie <charliekw@gmail.com>
2019-02-24 00:37:33 -0600

    timer debug

--- game.js
+++ game.js
@@ -1,16 +1,9 @@
-var box = document.getElementById('box'), // the box
-boxPos = 10, // the box's position
-boxVelocity = 2, // the box's velocity
-limit = 300; // how far the box can go before it switches direction
 
 function update() {
-    boxPos += boxVelocity;
-    // Switch directions if we go too far
-    if (boxPos >= limit || boxPos <= 0) boxVelocity = -boxVelocity;
+    console.log(Date.now());
 }
 
 function draw() {
-    box.style.left = boxPos + 'px';
 }
 
 
